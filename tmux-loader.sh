0274c10dbd77fc69375e2cb89ecb416f65f0576b
Yelson Chevarrias <chevarrias@gmail.com>
2022-03-24 21:58:12 -0500

    [ UPDATE ] tmux loader

--- tmux-loader.sh
+++ tmux-loader.sh
@@ -1,14 +1,17 @@
-#!/bin/sh
-source .venv/bin/activate
+#!/bin/zsh
 tmux new-session \; \
   set-window-option -g mouse on \; \
+  send-keys 'source .venv/bin/activate' C-m \; \
   send-keys './manage.py runserver' C-m \; \
   split-window -v \; \
+  send-keys 'source .venv/bin/activate' C-m \; \
   send-keys 'redis-cli -n 2 flushall' C-m \; \
   send-keys 'celery -A webapp worker -n celery@localhost --loglevel=INFO' C-m \; \
   split-window -h \; \
+  send-keys 'source .venv/bin/activate' C-m \; \
   send-keys 'celery -A webapp beat --loglevel=INFO' C-m \; \
   split-window -v \; \
+  send-keys 'nvm use v16.13.0' C-m \; \
   send-keys 'npx tailwindcss -i ./static/tailwind/main.css -o ./static/css/main.css --watch' C-m \;
 
 #tmux new-session -d 'web'
